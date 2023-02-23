d31d5d19700b75242a18cdb1cb17356f16311f86
khloe <slowykhloe@gmail.com>
2023-02-23 10:05:08 -0800

    initial commit added code 2/16 while loops notes and one 2/17 while loops two 2/21 for loops notes and one 2/23

---
+++ forLoops.js
@@ -0,0 +1,31 @@
+// for (let i = 0; i<=10; i++){
+//     console.log('hi');
+// }
+
+// for (let i = 0; i<=1000; i++){
+//     console.log(i);
+// }
+
+// for(let i = 100; i>=12; i--){
+// console.log(i);
+// }
+
+// for(let i = 100; i<= 300; i++){
+// if(i%2==0){
+// console.log(i);
+// }
+// }
+
+// let sum = 0;
+// for(let i=0; i<=100; i++){
+// sum+= i;
+// }
+// console.log(sum);
+
+let sum = 0;
+for(let i=0; i<=100; i++){
+    if(i%2==0){
+        sum+= i;
+    }
+}
+console.log(`sum: ${sum}`);
---
+++ forLoops1.js
@@ -0,0 +1,61 @@
+//1
+for (let a = 0; a<=150; a++){
+    console.log(a);
+}
+
+//2
+for (let b = 0; b<=40; b++){
+    console.log(b*b);
+}
+
+//3
+for (let c = 0; c<=40; c++){
+    console.log(c*c*c);
+}
+
+//4
+for(let d = 0; d <= 700; d++){
+    if(d%5==0){
+        console.log(d);
+    }
+}
+
+//5
+for(let e = 350; e <= 1000; e++){
+    if(e%10==0){
+        console.log(e);
+    }
+}
+
+//6
+let sum = 0;
+for(let f=1; f<=50; f++){
+    sum+= f;
+}
+console.log(sum);
+
+//7
+let sum = 0;
+for(let g=35; g<=70; g++){
+    sum+= g;
+}
+console.log(sum);
+
+//8
+let product = 1;
+for(let h=1; h<=15; h++){
+    product*= h;
+}
+console.log(product);
+
+//9
+let product = 1;
+for(let i=10; i<=35; i++){
+    product*= i;
+}
+console.log(product);
+
+//10
+for (let j = 0; j < 12; j++){
+    console.log('happy holidays');
+}
